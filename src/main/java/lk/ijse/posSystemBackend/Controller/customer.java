5aed2e0e2e05d3a0bcb92dcd15e20c04011b5c59
Kavindu Thareen <kavindutharin@gmail.com>
2024-08-02 18:23:16 +0530

    pos system backend customer PUT and GET done

--- src/main/java/lk/ijse/posSystemBackend/Controller/customer.java
+++ src/main/java/lk/ijse/posSystemBackend/Controller/customer.java
@@ -17,12 +17,22 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.swing.UIManager.getString;
 
 @WebServlet(urlPatterns = "/customer")
 public class customer extends HttpServlet {
 
     private Connection connection;
     private static final String SAVE_CUS = "INSERT INTO customer (id, name, address, salory) VALUES (?, ?, ?, ?)";
+    public static String GET_CUS = "SELECT * FROM customer WHERE id=?";
+    public static String UPDATE_CUS = "UPDATE customer SET name=?, address=?, salory=?, WHERE id=?";
+    public static String DELETE_CUS = "DELETE FROM customer WHERE id=?";
+
+    private static final String GET_ALL_CUS = "SELECT * FROM customer";
+
 
     @Override
     public void init() throws ServletException {
@@ -75,4 +85,45 @@ public class customer extends HttpServlet {
             e.printStackTrace();
         }
     }
+
+
+
+
+
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        try (var writer = resp.getWriter()) {
+            Jsonb jsonb = JsonbBuilder.create();
+            List<CustomerDTO> customers = new ArrayList<>();
+
+            try (var stmt = connection.createStatement();
+                 var rs = stmt.executeQuery(GET_ALL_CUS)) {
+                while (rs.next()) {
+                    CustomerDTO customerDTO = new CustomerDTO();
+                    customerDTO.setId(rs.getString("id"));
+                    customerDTO.setName(rs.getString("name"));
+                    customerDTO.setAddress(rs.getString("address"));
+                    customerDTO.setSalory(rs.getString("salory"));
+                    customers.add(customerDTO);
+                }
+            }
+
+            jsonb.toJson(customers, writer);
+        } catch (SQLException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            try (PrintWriter writer = resp.getWriter()) {
+                writer.write("{\"message\": \"Database error\"}");
+            }
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
+
+
